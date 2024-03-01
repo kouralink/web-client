96b03620ae9d054691ded7371d3423c4bf818ede
ilorez <zobirnajdaoui@gmail.com>
2024-03-01 19:18:28 +0100

    create head section compoenent for all homepage sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/global/Header";
 import Navbar from "./components/global/Navbar";
 import Privileges from "./components/global/Privileges";
+import SectionHead from "./components/global/SectionHead";
 
 export default function App() {
   return (
@@ -9,9 +10,13 @@ export default function App() {
         <Navbar />
       </div>
       <Header title="Welcome to Kouralink" src="/src/assets/bg.png" />
-      <div className="mb-8">
-      <Privileges />
+      <div className="">
+        <Privileges />
       </div>
+      <SectionHead
+        title="Section Title"
+        description="description Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure provident eum voluptates neque autem commodi cumque quod laudantium earum rem? Ab numquam asperiores sequi cumque expedita qui architecto maxime dolores."
+      />
     </div>
   );
 }
---
+++ src/components/global/SectionHead.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import SectionTitle from "./SectionTitle";
+
+interface SectionHeadProps {
+  title: string;
+  description: string;
+}
+
+const SectionHead: React.FC<SectionHeadProps> = ({ title, description }) => {
+  return (
+    <div className="flex w-fit h-[150px] items-center">
+      <div className="relative z-[-10] h-[400px] w-[100px]  ">
+        <img
+          src="/src/assets/SectionTitelFlish.svg"
+          alt="flish"
+          className="absolute  w-full h-full"
+        />
+      </div>
+      <div className="relative right-[70px]">
+        <h1 className="text-4xl font-bold">
+          {<SectionTitle color="text-green-900" title={title} />}
+        </h1>
+        <p className="max-w-[600px] pl-6">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SectionHead;
