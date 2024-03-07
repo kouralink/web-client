57e1b63ccdf979a5a641ae6c4a173d7e792d99a7
ilorez <zobirnajdaoui@gmail.com>
2024-03-07 13:15:57 +0100

    update navbar title add classesStyle

--- src/components/global/SectionTitle.tsx
+++ src/components/global/SectionTitle.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 interface SectionTitleProps {
     title: string;
-    color: string;
+    color?: string;
+    classesStyle?: string;
 }
 
-const SectionTitle: React.FC<SectionTitleProps> = ({ title, color }) => {
+const SectionTitle: React.FC<SectionTitleProps> = ({ title, color = 'text-primary-700',classesStyle = "text-3xl  sm:text-4xl md:text-5xl lg:text-6xl" }) => {
     const uppercasedTitle = title
         .split(' ')
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         ;
 
     return (
-        <h1 className="text-3xl  sm:text-4xl md:text-5xl lg:text-6xl">
+        <h1 className={classesStyle}>
             {uppercasedTitle.map((word, index) => (
                 <span key={index} className='' >
                 <span className={color}>{word.charAt(0)}</span>
