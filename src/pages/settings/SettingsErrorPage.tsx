31c19990b3722f4bc78f9cfffe0da45602ce1e31
ilorez <zobirnajdaoui@gmail.com>
2024-03-13 13:39:45 +0000

    create basic error page

---
+++ src/pages/settings/SettingsErrorPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SettingsErrorPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Error Page</h1>
+            <p>Oops! Something went wrong.</p>
+        </div>
+    );
+}
+
+export default SettingsErrorPage;
