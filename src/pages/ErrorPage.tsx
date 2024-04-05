5601d5fab927200acfad3b25fc5c3075a95af5d1
ilorez <zobirnajdaoui@gmail.com>
2024-04-05 18:19:51 +0000

    edit errorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage: React.FC = () => {
     return (
         <div>
             <h1>Error Page</h1>
             <p>Oops! Something went wrong.</p>
+            <Link to="/" className='text-sky-700'>
+                Go to Home Page
+            </Link>
         </div>
     );
 }
