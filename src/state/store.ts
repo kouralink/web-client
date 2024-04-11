a6193ea450ced60bf331ee17c59c7f2129e03570
ilorez <zobirnajdaoui@gmail.com>
2024-04-11 10:40:29 +0000

    add teamSlice to redux store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth/authSlice";
+import teamSlice from "./team/teamSlice";
 
 export const store = configureStore({
     reducer:{
         auth:authSlice,
+        team:teamSlice,
     },
 });
 export type RootState = ReturnType<typeof store.getState>;
