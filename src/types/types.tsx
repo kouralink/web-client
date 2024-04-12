978474d367c10a7a7d0b997bfcca8efb64672a57
ilorez <zobirnajdaoui@gmail.com>
2024-04-12 09:30:48 +0000

    Refactor teamSlice.ts and types.tsx to improve code organization and readability

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,7 +1,31 @@
 import dynamicIconImports from "lucide-react/dynamicIconImports";
 
 export type sidebarNavItemType = {
-    title: string;
-    href: string;
-    icon: keyof typeof dynamicIconImports;
-    };
+  title: string;
+  href: string;
+  icon: keyof typeof dynamicIconImports;
+};
+
+export interface TimeStamp {
+  seconds: number;
+  nanoseconds: number;
+}
+export interface Team {
+  id: string;
+  teamName: string;
+  blackList: string[];
+  coach: string;
+  createdAt: TimeStamp;
+  updatedAt: TimeStamp;
+  teamLogo: string | null;
+  description: string | null;
+  createdBy: string;
+}
+
+export interface TeamState {
+  team: Team;
+  status: "idle" | "loading" | "failed";
+  error: string | null | undefined;
+}
+
+
