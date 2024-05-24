9e57e9bf6769e9b7cf81f9769df3f618b1501340
ilorez <zobirnajdaoui@gmail.com>
2024-05-24 21:38:55 +0100

    Refactor UserProfile component to display user's birthday using a Timestamp object

--- src/pages/profile/UserProfile.tsx
+++ src/pages/profile/UserProfile.tsx
@@ -1,5 +1,6 @@
 import { AppDispatch, RootState } from "@/state/store";
 import { getUserByUsername } from "@/state/user/userSlice";
+import { Timestamp } from "firebase/firestore";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -22,6 +23,8 @@ export default function UserProfile() {
   useEffect(() => {
     dispatch(getUserByUsername(username));
   }, [dispatch, username]);
+
+  const timestamp = new Timestamp(authUser?.birthday?.seconds || 0, authUser?.birthday?.nanoseconds || 0);
   return (
     <div>
       
@@ -30,7 +33,7 @@ export default function UserProfile() {
           <li>username: {userState.username}</li>
           <li>accountType: {userState.accountType}</li>
           <li>bio: {userState.bio}</li>
-          <li>birthday: {userState.birthday}</li>
+          <li>birthday: {timestamp.toDate().toDateString()}</li>
           <li>firstName: {userState.firstName}</li>
           <li>lastName: {userState.lastName}</li>
           <li>Gender: {userState.gender}</li>
