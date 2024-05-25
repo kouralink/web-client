b3c3af242cd765d34d19b4e47a492d67d034e9f3
ilorez <zobirnajdaoui@gmail.com>
2024-05-25 15:45:25 +0100

    refactor: Update MemberCard component to use userInfo object for avatar and username

--- src/components/global/cards/MemberCard.tsx
+++ src/components/global/cards/MemberCard.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { MemberState } from "@/types/types";
+import { Member } from "@/types/types";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { MemberDropDownMenu } from "../MemberDropDownMenu";
 
-const MemberCard: React.FC<MemberState> = (props) => {
+const MemberCard: React.FC<Member> = (props) => {
   return (
     <Card
       className={"flex w-[280px] rounded-lg p-2 justify-between items-center"}
@@ -12,10 +12,10 @@ const MemberCard: React.FC<MemberState> = (props) => {
       <CardHeader className="flex flex-row  items-center justify-center gap-4 m-0 p-0 ">
         <div className="flex  items-center gap-2">
           <Avatar className="w-8 h-8">
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>{props.username.charAt(0)}</AvatarFallback>
+            <AvatarImage src={props.userInfo?.avatar} alt={props.userInfo?.username || " a " + " avatar"} />
+            <AvatarFallback>{props.userInfo?.username.charAt(0)}</AvatarFallback>
           </Avatar>
-          <h2>{props.username}</h2>
+          <h2>{props.userInfo?.username}</h2>
         </div>
       </CardHeader>
       <CardContent className="flex items-center justify-center gap-4 m-0 p-0">
