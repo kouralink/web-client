f53b40c70d146c01fc6f3362fa8c054093446de7
hamza wahmane <hamza.wahm5@gmail.com>
2024-05-29 00:17:55 +0100

    update

--- src/pages/team/TeamSearchPage.tsx
+++ src/pages/team/TeamSearchPage.tsx
@@ -1,9 +1,11 @@
 import { searchByTeamName } from '@/state/search/searchTeamSlice';
 import { AppDispatch, RootState } from '@/state/store';
 import { useDispatch, useSelector } from 'react-redux';
-import teams from '/teams.jpg'; // adjust the path as needed
 import { Input } from "@/components/ui/input"
 import { Link } from 'react-router-dom';
+import TeamSearchCard from "@/components/global/cards/TeamSearchCard";
+import TeamImg from '/teams.jpg';
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 
 const TeamSearchPage: React.FC = () => {
@@ -16,23 +18,39 @@ const TeamSearchPage: React.FC = () => {
     };
 
     return (
-        <div className=' flex justify-between w-full h-full'>
-            <div className='w-1/2 h-screen'>
+        <div className="container h-screen relative  flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r ">
+          {/* bg iamge url */}
+          <div className="absolute inset-0 bg-primary-950 bg-cover bg-no-repeat bg-center "  style={{
+            backgroundImage: `url(${TeamImg})`
+          }}/>
+          <div className="absolute inset-0 backdrop-brightness-50 "/>
+
+        </div>
+        <ScrollArea className="h-full w-full rounded-md border">
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col space-y-6 ">
+            
             <h1 className='font-bold text-7xl py-10'><span className='text-green-700'>T</span>eams</h1>
-            <p className='text-gray-600 text-2xl w-1/2'>search and Join your team to collaborate with passionate individuals and enjoy a fulfilling experience together</p>
+            <p className='text-gray-600 lg:text-2xl lg-w-1/2'>search and Join your team to collaborate with passionate individuals and enjoy a fulfilling experience together</p>
             <div className='flex flex-col p-10'>
             <Input type="text" onChange={handleSearch} placeholder="Search teams..." />
             <ul>
-                {searchResults.map((result, i) => {
-                        return <Link to={`/team/page/${result.team_info.teamName}`}><li key={i}> Team: {result.team_info.teamName }</li></Link>;
+                {searchResults.map((result) => {
+                        return <div>
+                                <Link to={`/users/profile/${result.team_info.teamName}`}>
+                                    <TeamSearchCard result={result.team_info}/>
+                                </Link>
+                            </div>
                 })}
             </ul>
-            </div>
-            </div>
+            </div>            
 
-            <div style={{ backgroundImage: `url(${teams})` }} className='w-1/2 h-screen bg-cover bg-repeat bg'>
-            </div>
+          </div>
         </div>
+        </ScrollArea>
+      </div>
+       
     );
 };
 
