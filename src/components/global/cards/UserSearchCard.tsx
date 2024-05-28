793dcc1dfa3928ca7ab7acfdf4a070c18dd9e883
Zobair Najdaoui <117188991+ilorez@users.noreply.github.com>
2024-05-29 00:28:39 +0100

    Update UserSearchCard.tsx

--- src/components/global/cards/UserSearchCard.tsx
+++ src/components/global/cards/UserSearchCard.tsx
@@ -1,17 +1,12 @@
 import { Separator } from "@/components/ui/separator"
+import { User } from "@/types/types";
 
 
 
-interface SearchedUser {
-    firstName: string;
-    username: string;
-    lastName: string;
-    avatar?: string;
 
-}
 
 interface UserSearchCardProps {
-    result: SearchedUser;
+    result: User;
 }
 
 const UserSearchCard: React.FC<UserSearchCardProps> = ({result}) => {
@@ -19,7 +14,7 @@ const UserSearchCard: React.FC<UserSearchCardProps> = ({result}) => {
             <div>
                     <div className="flex items-center p-5">
                     {result.avatar ? <img src={result.avatar} alt="" className="w-24 h-24 rounded-full" />: <div className="rounded-full bg-gray-300 w-24 h-24"></div>}
-                        <h1 className="flex flex-col p-5 font-bold ">{result.firstName} {result.lastName}</h1> 
+                        <h1 className="flex flex-col p-5 font-bold ">{result?.firstName} {result?.lastName}</h1> 
                 </div>
                 <Separator />
             </div>
