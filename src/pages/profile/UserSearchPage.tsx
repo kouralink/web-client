8a23be8da9fb67b03b06e234dfc54480bcfc93b6
ilorez <zobirnajdaoui@gmail.com>
2024-05-29 20:47:11 +0100

    shout some default users and teams in search page. add button view profile

--- src/pages/profile/UserSearchPage.tsx
+++ src/pages/profile/UserSearchPage.tsx
@@ -1,55 +1,65 @@
-import { searchByUserName } from '@/state/search/searchUsersSlice';
-import { AppDispatch, RootState } from '@/state/store';
-import { useDispatch, useSelector } from 'react-redux';
-import { Input } from "@/components/ui/input"
-import UsersImg from '/feautreContent/im5.png';
+import { searchByUserName } from "@/state/search/searchUsersSlice";
+import { AppDispatch, RootState } from "@/state/store";
+import { useDispatch, useSelector } from "react-redux";
+import { Input } from "@/components/ui/input";
+import UsersImg from "/feautreContent/im5.png";
 import UserSearchCard from "@/components/global/cards/UserSearchCard";
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useEffect, useState } from "react";
 
 const UserSearchPage: React.FC = () => {
-    const dispatch = useDispatch<AppDispatch>();
-    const searchResults = useSelector((state: RootState) => state.usersearch.searchResults);
-    
-    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-        dispatch(searchByUserName(event.target.value));
-        // Perform search logic here
-    };
+  const dispatch = useDispatch<AppDispatch>();
+  const searchResults = useSelector(
+    (state: RootState) => state.usersearch.searchResults
+  );
+  const [searchValue, setSearchValue] = useState<string>("");
 
-    return (
-    
-        <div className="container h-screen relative  flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r ">
-          {/* bg iamge url */}
-          <div className="absolute inset-0 bg-primary-950 bg-cover bg-no-repeat bg-center "  style={{
-            backgroundImage: `url(${UsersImg})`
-          }}/>
-          <div className="absolute inset-0 backdrop-brightness-50 "/>
+  useEffect(() => {
+    dispatch(searchByUserName(searchValue));
+  }, [dispatch, searchValue]);
 
-        </div>
-        <ScrollArea className="h-full w-full rounded-md border">
+  return (
+    <div className="container h-screen relative  flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r ">
+        {/* bg iamge url */}
+        <div
+          className="absolute inset-0 bg-primary-950 bg-cover bg-no-repeat bg-center "
+          style={{
+            backgroundImage: `url(${UsersImg})`,
+          }}
+        />
+        <div className="absolute inset-0 backdrop-brightness-50 " />
+      </div>
+      <ScrollArea className="h-full w-full rounded-md border">
         <div className="lg:p-8">
-            <div className="mx-auto flex w-full flex-col space-y-6 ">
+          <div className="mx-auto flex w-full flex-col space-y-6 ">
+            <h1 className="font-bold text-7xl">
+              <span className="text-green-700">U</span>sers
+            </h1>
+            <p className="text-gray-600 lg:text-2xl lg-w-1/2">
+              search and players and enjoy a fulfilling experience together
+            </p>
+            <Input
+              type="text"
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search users..."
+              className="my-10"
+            />
 
-
-        <h1 className='font-bold text-7xl'><span className='text-green-700'>U</span>sers</h1>
-        <p className='text-gray-600 lg:text-2xl lg-w-1/2'>search and players and enjoy a fulfilling experience together</p>
-        <Input type="text" onChange={handleSearch} placeholder="Search users..." className='my-10' />
-        
-            <div className='flex flex-col'>
-
-                {searchResults.map((result) => {
-                            return <div>
-                                    <UserSearchCard result={result.user_info}/>
-                                </div>;
-                    })}
+            <div className="flex flex-col">
+              {searchResults.map((result) => {
+                return (
+                  <div>
+                    <UserSearchCard result={result.user_info} />
+                  </div>
+                );
+              })}
             </div>
-
+          </div>
         </div>
-        </div>
-        </ScrollArea>
-        </div>
-        
-    );
+      </ScrollArea>
+    </div>
+  );
 };
 
 export default UserSearchPage;
