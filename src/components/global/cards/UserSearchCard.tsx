60361e0478a3acc8431b5f5676e90bb4fa8717fd
hamza wahmane <hamza.wahm5@gmail.com>
2024-05-29 01:56:17 +0100

    update

--- src/components/global/cards/UserSearchCard.tsx
+++ src/components/global/cards/UserSearchCard.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@/components/ui/separator"
 import { User } from "@/types/types";
-
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom";
 
 
 
@@ -12,9 +13,14 @@ interface UserSearchCardProps {
 const UserSearchCard: React.FC<UserSearchCardProps> = ({result}) => {
     return (
             <div>
-                    <div className="flex items-center p-5">
-                    {result.avatar ? <img src={result.avatar} alt="" className="w-24 h-24 rounded-full" />: <div className="rounded-full bg-gray-300 w-24 h-24"></div>}
-                        <h1 className="flex flex-col p-5 font-bold ">{result?.firstName} {result?.lastName}</h1> 
+                <div className="flex justify-between items-center">
+                    <Link to={`/users/profile/${result.username}`}>
+                        <div className="flex items-center p-5">
+                        {result.avatar ? <img src={result.avatar} alt="" className="w-24 h-24 rounded-full" />: <div className="rounded-full bg-gray-300 w-24 h-24"></div>}
+                            <h1 className="flex flex-col p-5 font-bold ">{result?.firstName} {result?.lastName}</h1> 
+                        </div>
+                    </Link>
+                    <Button>Send invite</Button>
                 </div>
                 <Separator />
             </div>
