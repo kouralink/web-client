35454a71d23691ed24d6ea0ba47ace5923d711c2
ilorez <zobirnajdaoui@gmail.com>
2024-06-02 13:08:05 +0100

    feat: Add MatchTeamDropDownMenu component for displaying dropdown menu in match cards

---
+++ src/components/global/match/cards/MatchTeamDropDownMenu.tsx
@@ -0,0 +1,67 @@
+import { Group, LogOut } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Link } from "react-router-dom";
+
+export default function MatchTeamDropDownMenu({
+  teamname,
+  role,
+}: {
+  teamname: string;
+  role: "user" | "coach" | "member";
+}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-ellipsis-vertical"
+        >
+          <circle cx="12" cy="12" r="1" />
+          <circle cx="12" cy="5" r="1" />
+          <circle cx="12" cy="19" r="1" />
+        </svg>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Match Team Menu</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {role === "coach" && (
+          <DropdownMenuGroup>
+            <DropdownMenuItem className="text-red-500" asChild>
+              <LogOut className="mr-2 h-4 w-4" />
+              {/* // TODO: add confirm dialog  */}
+              <span>Leave Match</span>
+              <DropdownMenuShortcut></DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        )}
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link to={`/team/page/${teamname}`}>
+              <Group className="mr-2 h-4 w-4" />
+              <span>Team Page</span>
+              <DropdownMenuShortcut></DropdownMenuShortcut>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
