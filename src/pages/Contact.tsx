707c1a1d36e5c53fdd52e73dbbd92b698a57e785
hamza wahmane <hamza.wahm5@gmail.com>
2024-06-03 21:49:56 +0100

    fix contact media

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,7 +5,7 @@ import ContactFormCard from "@/components/global/cards/ContactFormCard"
 
 const Contact = () => {
   return (
-    <div className="w-full maxh-screen lg:h-screen flex flex-col items-center">
+    <div className="w-full maxh-screen lg:h-fit flex flex-col items-center">
       <div className="py-12">
         <SectionTitle
           title="Contact Us"
@@ -14,16 +14,16 @@ const Contact = () => {
         <h2 className="text-gray-400">Any question or remarks? Just write us a message!</h2>
       </div>
 
-      <div className="w-full h-5/6 lg:w-2/3 lg:h-2/3  flex flex-col lg:flex-row ">
+      <div className="w-full h-fit lg:w-2/3 lg:h-fit  flex flex-col lg:flex-row ">
 
-        <div className="w-full lg:w-1/3 h-full bg-green-500 flex flex-col items-center lg:items-stretch justify-around px-11 rounded-t-2xl lg:rounded-none lg:rounded-l-3xl" >
+        <div className="w-full lg:w-1/3  bg-green-500 flex flex-col items-center lg:items-stretch justify-around px-11 rounded-t-2xl lg:rounded-none lg:rounded-l-3xl" >
           <div className="flex flex-col justify-center items-center py-2">
             <h1 className="text-3xl font-bold text-white">Contact Information</h1>
             <h2 className="text-gray-50">Say something to start a live chat!</h2>
           </div>
           <div className="h-1/3 flex flex-col justify-between ">
-            <div className="flex flex-col lg:flex-row  justify-center items-center lg:justify-start py-5 text-white"><Phone /><span className="px-5" >+212 6 00 00 00 00</span></div>
-            <div className="flex flex-col lg:flex-row  justify-center items-center lg:justify-start py-5 text-white"><Mail /><span className="px-5" >example@gmail.com</span></div>
+            <div className="flex flex-col lg:flex-row  justify-center items-center lg:justify-start py-5 text-white"><Phone /><span className="px-5" >06 15 33 1474</span></div>
+            <div className="flex flex-col lg:flex-row  justify-center items-center lg:justify-start py-5 text-white"><Mail /><span className="px-5" >kouralink@gmail.com</span></div>
             <div className="flex flex-col lg:flex-row  justify-center items-center lg:justify-start py-5 text-white"><MapPin /><span className="px-5" >Ista Ntic Syba</span></div>
           </div>
 
