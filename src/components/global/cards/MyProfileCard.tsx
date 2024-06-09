9a7073910a9197032a08a90d4fc9a60646ef8552
hamza wahmane <hamza.wahm5@gmail.com>
2024-06-09 17:23:26 +0100

    profile team

--- src/components/global/cards/MyProfileCard.tsx
+++ src/components/global/cards/MyProfileCard.tsx
@@ -9,8 +9,11 @@ import { Bolt } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useMatch } from "react-router-dom";
 import { inviteToTeam } from "@/state/notification/notificationSlice";
+import TeamProfileCard from "@/components/global/cards/TeamProfileCard"
+
 
 const MyProfileCard = () => {
+  const team = useSelector((state: RootState) => state.team.team);
   const userS = useSelector((state: RootState) => state.user);
   const authU = useSelector((state: RootState) => state.auth);
   const userState = userS.user;
@@ -32,7 +35,7 @@ const MyProfileCard = () => {
       <CardHeader>
         <div className="flex items-center justify-between py-5">
           <p className="text-3xl lg:text-8xl font-bold capitalize">
-            {userState.firstName} {userState.lastName}
+            {userState.firstName} {userState.lastName} <span className="text-5xl">({authUser?.accountType})</span>
           </p>
           {match && (
             <Link to="/settings">
@@ -44,53 +47,58 @@ const MyProfileCard = () => {
           {userState.bio ? userState.bio : "add bio in settings"}
         </div>
       </CardHeader>
-      <CardContent>
-        <ul className="space-y-5">
-          <li className="flex items-center py-3">
-            <BookUser />
-            <span className="px-4">{userState.username}</span>
-          </li>
-          <li className="flex items-center py-3">
-            <Cake />
-            <span className="px-4">
-              {timestamp.toDate().toDateString()
-                ? timestamp.toDate().toDateString()
-                : "add birthday in settings"}
-            </span>
-          </li>
-          <li className="flex items-center py-3">
-            <Smile />
-            <span className="px-4">
-              {userState.gender ? userState.gender : "add gender in settings"}
-            </span>
-          </li>
-          <li className="flex items-center py-3">
-            <Phone />
-            <span className="px-4 flex gap-2">
-              {userState.phoneNumbers
-                ? userState.phoneNumbers.map((n)=>{
-                  return <Card key={n} className="px-2 py-1">{n}</Card>
-                })
-                : "add phone number in settings"}
-            </span>
-          </li>
-          <li className="flex items-center py-3">
-            <Home />
-            <span className="px-4">
-              {userState.address
-                ? userState.address
-                : "add address in settings"}
-            </span>
-          </li>
-        </ul>
-        {authUser?.accountType === "coach" && authU.uid !== userS.uid && (
-          <div className="w-full flex justify-end">
-            <Button onClick={handelInvite}>
-              {" "}
-              <UserPlus className="mr-2 h-4 w-4" /> Invite To Team
-            </Button>
-          </div>
-        )}
+      <CardContent className="flex flex-col lg:flex-row justify-between">
+        <div>
+          <ul className="space-y-5">
+            <li className="flex items-center py-3">
+              <BookUser />
+              <span className="px-4">{userState.username}</span>
+            </li>
+            <li className="flex items-center py-3">
+              <Cake />
+              <span className="px-4">
+                {timestamp.toDate().toDateString()
+                  ? timestamp.toDate().toDateString()
+                  : "add birthday in settings"}
+              </span>
+            </li>
+            <li className="flex items-center py-3">
+              <Smile />
+              <span className="px-4">
+                {userState.gender ? userState.gender : "add gender in settings"}
+              </span>
+            </li>
+            <li className="flex items-center py-3">
+              <Phone />
+              <span className="px-4 flex gap-2">
+                {userState.phoneNumbers
+                  ? userState.phoneNumbers.map((n) => {
+                    return <Card key={n} className="px-2 py-1">{n}</Card>
+                  })
+                  : "add phone number in settings"}
+              </span>
+            </li>
+            <li className="flex items-center py-3">
+              <Home />
+              <span className="px-4">
+                {userState.address
+                  ? userState.address
+                  : "add address in settings"}
+              </span>
+            </li>
+          </ul>
+          {authUser?.accountType === "coach" && authU.uid !== userS.uid && (
+            <div className="w-full flex justify-end">
+              <Button onClick={handelInvite}>
+                {" "}
+                <UserPlus className="mr-2 h-4 w-4" /> Invite To Team
+              </Button>
+            </div>
+          )}
+        </div>
+        <div className=" lg:w-1/2">
+          <TeamProfileCard team={team}/>
+        </div>
       </CardContent>
     </Card>
   );
