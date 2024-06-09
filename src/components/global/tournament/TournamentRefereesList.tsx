dc72d2f516aa5d31b4c518d993d6b84c80182625
ilorez <zobirnajdaoui@gmail.com>
2024-06-09 12:18:06 +0100

    refactor: Update TournamentRefereesList component
    Refactor the `TournamentRefereesList` component to use the `referees` prop instead of `referee` for consistency with the naming convention used throughout the codebase. This change ensures that the component correctly renders the list of referees and handles the case when there are no referees.

--- src/components/global/tournament/TournamentRefereesList.tsx
+++ src/components/global/tournament/TournamentRefereesList.tsx
@@ -1,26 +1,25 @@
-import {  User } from "@/types/types";
+import { User } from "@/types/types";
 import { Card } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import RefreeCard from "./cards/RefreeCard";
 
 interface PropsState {
-  referee: User[];
+  referees: User[];
 }
 
 export default function TournamentRefereesList(props: PropsState) {
   return (
-    <Card className=" flex flex-col py-4 px-2 gap-4 w-full h-full">
+    <Card className=" flex flex-col py-6 px-2 gap-4 w-full h-full ">
       <div className="flex flex-col gap-2 w-full">
         <h2>Referees List</h2>
-        <div className=" ml-4">
-          <ScrollArea className="min-h-52 h-full w-full">
-            {props.referee.map((referee) => {
-              return <RefreeCard key={referee.username} userInfo={referee} />;
-            })}
-            {props.referee.length === 0 && (
+        <div className=" ml-4 flex">
+            <div className="h-full w-full flex flex-wrap gap-2">
+              {props.referees.map((referee) => (
+                <RefreeCard key={referee.username} userInfo={referee} />
+              ))}
+            </div>
+            {props.referees.length === 0 && (
               <p className="text-muted-foreground">No Refrees yet</p>
             )}
-          </ScrollArea>
         </div>
       </div>
     </Card>
