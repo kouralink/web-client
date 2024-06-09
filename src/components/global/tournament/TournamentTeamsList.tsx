da499441fde41a15445c2d2305378f351869f4fc
ilorez <zobirnajdaoui@gmail.com>
2024-06-09 12:18:13 +0100

    feat: Update TournamentTeamsList component
    Refactor the `TournamentTeamsList` component to improve the layout and naming consistency. Update the title to "Teams List" and remove the unnecessary `ScrollArea` component. This change ensures a better user experience and simplifies the code structure.

--- src/components/global/tournament/TournamentTeamsList.tsx
+++ src/components/global/tournament/TournamentTeamsList.tsx
@@ -1,6 +1,5 @@
 import {  Team } from "@/types/types";
 import { Card } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import TeamCard from "./cards/TeamCard";
 
 interface PropsState {
@@ -10,21 +9,20 @@ interface PropsState {
 
 export default function TournamentTeamsList(props: PropsState) {
   return (
-    <Card className=" flex flex-col py-4 px-2 gap-4 w-full h-full">
+    <Card className=" flex flex-col py-6 px-2 gap-4 w-full h-full">
           <div className="flex flex-col gap-2 w-full">
-            <h2>Teams</h2>
+            <h2>Teams List</h2>
             <div className=" ml-4">
-              <ScrollArea className="min-h-52 h-full w-full">
+              <div className="h-full w-full flex flex-wrap gap-2">
                 {props.teams.map((team) => {
                     return <TeamCard key={team.id} team={team} role={props.isManager ? "tournament_manager":"user"} />;
                 })}
                 {props.teams.length === 0 && (
                   <p className="text-muted-foreground">No teams yet</p>
                 )}
-              </ScrollArea>
+              </div>
             </div>
           </div>
-
     </Card>
   );
 }
