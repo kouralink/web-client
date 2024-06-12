bc208f19779767a4c2d306d81a4ace02ea5d1bcc
KAMADO <abdonsila2004@gmail.com>
2024-06-12 03:36:31 +0100

    feat: Improve MatchMemberCard component
    This commit improves the MatchMemberCard component by adding a hover effect and a link to the user's profile page. It enhances the visual appearance and user experience of the component.

--- src/components/global/match/cards/MatchMemberCard.tsx
+++ src/components/global/match/cards/MatchMemberCard.tsx
@@ -10,30 +10,30 @@ type MatchMemberCardProps = {
 
 const MatchMemberCard: React.FC<MatchMemberCardProps> = (props) => {
   return (
-    <Card
-      className={"flex w-[280px] rounded-lg p-2 justify-between items-center gap-6"}
-    >
-      <CardHeader className="flex flex-row  items-center justify-center gap-4 m-0 p-0 ">
-        <div className="flex  items-center gap-2">
-          <Avatar className="w-8 h-8">
-            <AvatarImage
-              src={props.member.userInfo?.avatar}
-              alt={props.member.userInfo?.username || " avatar"}
-              className="object-cover"
-            />
-            <AvatarFallback>
-              {props.member.userInfo?.username.charAt(0)}
-            </AvatarFallback>
-          </Avatar>
-          <h2>{props.member.userInfo?.username}</h2>
-        </div>
-      </CardHeader>
-      <CardContent className="flex items-center justify-center gap-4 m-0 p-0">
-        <Link to={`/users/profile/${props.member.userInfo?.username}`}>
+    <Link to={`/users/profile/${props.member.userInfo?.username}`}>
+      <Card
+        className={"flex w-[280px] rounded-lg p-2 justify-between items-center gap-6 hover:bg-secondary"}
+      >
+        <CardHeader className="flex flex-row  items-center justify-center gap-4 m-0 p-0 ">
+          <div className="flex  items-center gap-2">
+            <Avatar className="w-8 h-8">
+              <AvatarImage
+                src={props.member.userInfo?.avatar}
+                alt={props.member.userInfo?.username || " avatar"}
+                className="object-cover"
+              />
+              <AvatarFallback>
+                {props.member.userInfo?.username.charAt(0)}
+              </AvatarFallback>
+            </Avatar>
+            <h2>{props.member.userInfo?.username}</h2>
+          </div>
+        </CardHeader>
+        <CardContent className="flex items-center justify-center gap-4 m-0 p-0">
           <User className="mr-2 h-4 w-4" />
-        </Link>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 
