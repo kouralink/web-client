2ac0ff1a3bc0b5012501904047a34f3e0fdbdd35
KAMADO <abdonsila2004@gmail.com>
2024-06-12 01:03:44 +0100

    feat: Add lazy loading for team matches history

--- src/pages/team/TeamSearchPage.tsx
+++ src/pages/team/TeamSearchPage.tsx
@@ -12,6 +12,9 @@ const TeamSearchPage: React.FC = () => {
   const searchResults = useSelector(
     (state: RootState) => state.teamsearch.searchResults
   );
+  const isLoading = useSelector(
+    (state: RootState) => state.teamsearch.isLoading
+  );
   const [searchValue, setSearchValue] = useState<string>("");
 
   useEffect(() => {
@@ -46,15 +49,21 @@ const TeamSearchPage: React.FC = () => {
                 onChange={(e) => setSearchValue(e.target.value)}
                 placeholder="Search teams..."
               />
-              <ul>
-                {searchResults.map((result, index) => {
-                  return (
-                    <div key={index}>
-                      <TeamSearchCard result={result.team_info} />
-                    </div>
-                  );
-                })}
-              </ul>
+              {
+                isLoading ?
+                  <div className='h-full w-full flex justify-center items-center'>
+                    <img src="/logo.svg" className="h-8 me-3 mt-36 animate-spin" alt="Koulaink Logo" />
+                  </div> :
+                  <ul>
+                    {searchResults.map((result, index) => {
+                      return (
+                        <div key={index}>
+                          <TeamSearchCard result={result.team_info} />
+                        </div>
+                      );
+                    })}
+                  </ul>
+              }
             </div>
           </div>
         </div>
