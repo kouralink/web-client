aa9932cfa8c529895c0c973f36bc780debd9d679
KAMADO <abdonsila2004@gmail.com>
2024-06-22 00:22:10 +0100

    refactor: Update UserSearchCard component
    Refactor the UserSearchCard component to improve code cleanliness and remove unnecessary code. This component is no longer used in the project and has been replaced by the SearchCard component, which enhances the search functionality by displaying search results with a title, name, and avatar, along with a button for further actions.

---
+++ src/components/global/old/UserSearchCard.tsx
@@ -0,0 +1,35 @@
+import { Separator } from "@/components/ui/separator";
+import { User } from "@/types/types";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+interface UserSearchCardProps {
+  result: User;
+}
+
+const UserSearchCard: React.FC<UserSearchCardProps> = ({ result }) => {
+  return (
+    <div>
+      <Link to={`/users/profile/${result.username}`}>
+        <div className="flex justify-between items-center px-5">
+          <div className="flex items-center py-5 gap-2">
+
+            <Avatar className="w-24 h-24 rounded-full object-cover">
+              <AvatarImage src={result.avatar} alt="" className="object-cover" />
+              <AvatarFallback>{result.username.charAt(0).toUpperCase()}{result.username.charAt(1).toUpperCase()}</AvatarFallback>
+            </Avatar>
+
+            <h1 className="flex flex-col font-bold px-1 capitalize">
+              {result.username}
+            </h1>
+          </div>
+          <Button>View Profile</Button>
+        </div>
+      </Link>
+      <Separator />
+    </div>
+  );
+};
+
+export default UserSearchCard;
