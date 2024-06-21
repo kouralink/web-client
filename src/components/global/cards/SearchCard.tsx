a014b77d2ee581c2d167efff2cbefa3fc8fe3cdd
KAMADO <abdonsila2004@gmail.com>
2024-06-22 00:19:28 +0100

    refactor: Add SearchCard component
    Add the SearchCard component to the project. This component displays search results with a title, name, and avatar. It also includes a button for further actions. This addition enhances the search functionality and improves the user experience.

---
+++ src/components/global/cards/SearchCard.tsx
@@ -0,0 +1,36 @@
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
+interface SearchCardProps {
+  path: string;
+  title: string;
+  name: string;
+  avatar: string;
+}
+
+const SearchCard: React.FC<SearchCardProps> = ({ path, title, name, avatar }) => {
+  return (
+    <div>
+      <Link to={`${path}`}>
+        <div className="flex justify-between items-center ">
+          <div className="flex items-center p-5">
+
+            <Avatar className="w-24 h-24 rounded-full object-cover">
+              <AvatarImage src={avatar} alt="" className="object-cover" />
+              <AvatarFallback>{name.charAt(0).toUpperCase()}{name.charAt(1).toUpperCase()}</AvatarFallback>
+            </Avatar>
+
+            <h1 className="flex flex-col p-5 font-bold ">{name}</h1>
+          </div>
+          <Button>{title}</Button>
+        </div>
+      </Link>
+      <Separator />
+    </div>
+  );
+};
+
+export default SearchCard;
